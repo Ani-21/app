d6f8cc29319e8a761815d8588e989d604b76f680
Ani-21 <kvikviniya.am@edu.spbstu.ru>
2023-04-24 18:14:04 +0300

    feat: add logger

--- apps/server/dist/src/index.js
+++ apps/server/dist/src/index.js
@@ -16,10 +16,7 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = 8080;
 app.get('/', (req, res) => {
-    res.send('My server');
-});
-app.get('/api/v1', (req, res) => {
-    res.send('server');
+    res.send('Posts App Server');
 });
 app.get('/api/posts', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
@@ -28,7 +25,7 @@ app.get('/api/posts', (req, res) => __awaiter(void 0, void 0, void 0, function* 
         res.json(data);
     }
     catch (err) {
-        console.log(err);
+        throw new Error();
     }
 }));
 app.get('/api/posts/:id/comments', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -39,7 +36,7 @@ app.get('/api/posts/:id/comments', (req, res) => __awaiter(void 0, void 0, void 
         res.json(data);
     }
     catch (err) {
-        console.log(err);
+        throw new Error();
     }
 }));
 app.get('/api/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -49,30 +46,39 @@ app.get('/api/users', (req, res) => __awaiter(void 0, void 0, void 0, function* 
         res.json(data);
     }
     catch (err) {
-        console.log(err);
+        throw new Error();
     }
 }));
 app.get('/api/users/:id/posts', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const userId = Number(req.params.id);
-        const responsePosts = yield fetch(`https://jsonplaceholder.typicode.com/posts`);
+        const responsePosts = yield fetch('http://localhost:8080/api/posts');
         const dataPosts = yield responsePosts.json();
         const userPosts = dataPosts.filter(item => item.userId === userId);
         res.json(userPosts);
     }
     catch (err) {
-        console.log(err);
+        throw new Error();
     }
 }));
 app.get('/api/comments', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const response = yield fetch(`https://jsonplaceholder.typicode.com/comments`);
+        const response = yield fetch('https://jsonplaceholder.typicode.com/comments');
         const data = yield response.json();
         res.json(data);
     }
     catch (err) {
-        console.log(err);
+        throw new Error();
     }
 }));
-app.listen(PORT, () => console.log('start listening on port', PORT));
+app.post('/api/comments', (req, res) => {
+    try {
+        const data = req.body;
+        res.send(JSON.stringify(data));
+    }
+    catch (err) {
+        throw new Error();
+    }
+});
+app.listen(PORT);
 //# sourceMappingURL=index.js.map
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import { IComment, IPost, IUser } from './lib'
+import { logger } from './middlewares/logEvents'
 
 const app = express()
 const PORT = 8080
+
+app.use(logger)
 
 app.get('/', (req, res) => {
   res.send('Posts App Server')
@@ -10,8 +13,21 @@ app.get('/', (req, res) => {
 
 app.get('/api/posts', async (req, res) => {
   try {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data: IPost[] = await response.json()
+    res.json(data)
+  } catch (err) {
+    throw new Error()
+  }
+})
+
+app.get('/api/posts/:id', async (req, res) => {
+  try {
+    const postId = req.params.id
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${postId}`,
+    )
+    const data: IPost = await response.json()
     res.json(data)
   } catch (err) {
     throw new Error()
@@ -31,9 +47,30 @@ app.get('/api/posts/:id/comments', async (req, res) => {
   }
 })
 
+app.post('/api/posts/:id/comments', (req, res) => {
+  try {
+    const data: IComment = req.body
+    res.send(JSON.stringify(data))
+  } catch (err) {
+    throw new Error()
+  }
+})
+
 app.get('/api/users', async (req, res) => {
   try {
     const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    const data: IUser[] = await response.json()
+    res.json(data)
+  } catch (err) {
+    throw new Error()
+  }
+})
+app.get('/api/users/:id', async (req, res) => {
+  try {
+    const userId = req.params.id
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${userId}`,
+    )
     const data: IUser[] = await response.json()
     res.json(data)
   } catch (err) {
@@ -63,15 +100,6 @@ app.get('/api/comments', async (req, res) => {
     )
     const data: IComment[] = await response.json()
     res.json(data)
-  } catch (err) {
-    throw new Error()
-  }
-})
-
-app.post('/api/comments', (req, res) => {
-  try {
-    const data: IComment = req.body
-    res.send(JSON.stringify(data))
   } catch (err) {
     throw new Error()
   }
